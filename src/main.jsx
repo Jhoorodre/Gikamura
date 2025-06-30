d22bc631be6a9564d328e6f5bdb47ff60f4bcd18
Jhoorodre <secular-fox-opium@proton.me>
2025-06-30 18:30:59 -0300

    feat: Altera o roteador de BrowserRouter para HashRouter no componente principal

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AppProvider } from './context/AppContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         <AppProvider>
           <App />
         </AppProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
